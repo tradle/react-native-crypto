7e803f438ce3b252885d88b773303bdcc12cd0f1
Jason Douglas <jasond@me.com>
2017-09-19 22:38:05 -0700

    Define window.getRandomValues() if missing, and bump package version

--- index.js
+++ index.js
@@ -76,3 +76,20 @@ var publicEncrypt = require('public-encrypt')
     ].join('\n'))
   }
 })
+
+// implement window.getRandomValues(), for packages that rely on it
+if (typeof window === 'object') {
+  if (!window.crypto) window.crypto = {}
+  if (!window.crypto.getRandomValues) {
+    window.crypto.getRandomValues = function (arr) {
+      if (arr.byteLength != arr.length) {
+        // Get access to the underlying raw bytes
+        arr = new Uint8Array(arr.buffer)
+      }
+      const bytes = randomBytes(arr.length)
+      for (var i = 0; i < bytes.length; i++) {
+        arr[i] = bytes[i]
+      }
+    }
+  }
+}
